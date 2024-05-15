bc76c95115c8146a990b92e75bfdaf21d76729e3
jmgerdes1 <126284397+jmgerdes1@users.noreply.github.com>
2024-05-15 03:50:16 -0400

    Create complex_query_ex1.sql

---
+++ complex_query_ex1.sql
@@ -0,0 +1,23 @@
+with sales_history as (
+    SELECT
+        EXTRACT(YEAR FROM Date) AS YEAR,
+        EXTRACT(MONTH FROM Date) AS MONTH,
+        ProductID,
+        StoreID,
+        SUM(quantity) AS UnitsSold,
+        AVG(UnitPrice) AS UnitPriceProxy,
+        COUNT(DISTINCT salesID) AS NumTransactions
+    FROM
+        `proven-concept-421613.Sales.sales_info`
+    GROUP BY YEAR, MONTH, ProductId, StoreID
+)
+SELECT
+    inventory.*,
+    (
+        SELECT
+            AVG(UnitsSold) FROM sales_history
+        WHERE inventory.ProductID = sales_history.ProductID
+            AND inventory.StoreID = sales_history.ProductID
+    ) AS avg_quantity_sold_in_a_month
+FROM
+    `proven-concept-421613.Sales.inventory` AS inventory
